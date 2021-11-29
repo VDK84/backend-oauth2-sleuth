d0b0c83a80584c11254ecbd5ab7e02f0399dda90
VDK84 <valdreck@gmail.com>
2021-11-29 22:30:57 +0100

    add groovy script

---
+++ job/callJAva.groovy
@@ -0,0 +1,26 @@
+#
+#https://www.eficode.com/blog/jenkins-groovy-tutorial
+pipeline {
+    node any
+    
+
+    stages {
+        stage('Call WS') {
+            script {
+                callRESTapi()
+            }
+        }
+    }
+}
+
+def callRESTapi() {
+    def url = "${params.REST_URL}${params.REST_SERVICE}"
+    httpRequest(
+            acceptType: 'APPLICATION_JSON',
+            contentType: 'APPLICATION_JSON',
+            consoleLogResponseBody: DEBUG_HTTP,
+            ignoreSslErrors: true,
+            httpMode: 'GET',
+            url: url
+    )
+}
