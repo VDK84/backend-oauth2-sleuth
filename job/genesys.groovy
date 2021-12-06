e915d35efd26e8d16299947c5d412809de820f6d
VDK84 <valdreck@gmail.com>
2021-12-06 13:23:39 +0100

    add pipe parameters

--- job/genesys.groovy
+++ job/genesys.groovy
@@ -1,5 +1,9 @@
 //https://www.digitalocean.com/community/tutorials/how-to-automate-jenkins-job-configuration-using-job-dsl
 pipelineJob('test_rest3') {
+    parameters {
+            stringParam('REST_URL', '', 'REST API Base URL')
+            stringParam('REST_SERVICE', '', "REST service path")
+        }
     definition {
         cpsScm {
             scm {
